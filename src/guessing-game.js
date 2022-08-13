9050aec9f75eb44769c4f6c2c1cb706fd6ce5624
Svitlana <93516763+Svitlanka-K@users.noreply.github.com>
2022-08-13 20:31:57 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor(minNum, maxNum) {
+        this.minNum;
+        this.maxNum;
+}
 
-    setRange(min, max) {
+setRange(min, max) {
+        this.minNum = min;
+        this.maxNum = max;
+}
 
-    }
+guess() {
+        return Math.round(((this.maxNum - this.minNum) / 2) + this.minNum);
+}
 
-    guess() {
+lower() {
+    this.maxNum = Math.round(((this.maxNum - this.minNum) / 2) + this.minNum);
+}
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+greater() {
+    this.minNum = Math.round(((this.maxNum - this.minNum) / 2) + this.minNum);
+}
 }
 
 module.exports = GuessingGame;
